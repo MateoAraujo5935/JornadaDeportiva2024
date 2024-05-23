caa5d15a0bba8e41ee909480b6d23df59213e66a
MateoAraujo59 <emiliohector9@gmail.com>
2024-05-23 15:25:36 -0300

    countdown 2

--- js/countdown.js
+++ js/countdown.js
@@ -1,32 +1,39 @@
-const getRemainTime = deadline => {
-    let now = new Date(),
-        remainTime = (new Date(deadline) - now + 1000) /1000, 
-        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
-        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
-        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
-        remainDays = Math.floor(remainTime / (3600 * 24));
+const getRemainTime = (deadline) => {
+  let now = new Date(),
+    remainTime = (new Date(deadline) - now + 1000) / 1000,
+    remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2),
+    remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2),
+    remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2),
+    remainDays = Math.floor(remainTime / (3600 * 24));
 
-    return {
-        remainTime,
-        remainSeconds,
-        remainMinutes,
-        remainHours,
-        remainDays
-    }
+  return {
+    remainTime,
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays,
+  };
 };
 
 const countdown = (deadline, elem, finalMessage) => {
-    const el = document.getElementById(elem);
+  const el = document.getElementById(elem);
+  const daysSpan = document.getElementById("days");
+  const hoursSpan = document.getElementById("hours");
+  const minutesSpan = document.getElementById("minutes");
+  const secondsSpan = document.getElementById("seconds");
 
-    const timerupdate = setInterval( () => {
+  const timerupdate = setInterval(() => {
     let t = getRemainTime(deadline);
-    el.innerHTML = `${t.remainDays} D : ${t.remainHours} HS : ${t.remainMinutes} MINS`
+    daysSpan.textContent = t.remainDays;
+    hoursSpan.textContent = t.remainHours;
+    minutesSpan.textContent = t.remainMinutes;
+    secondsSpan.textContent = t.remainSeconds;
 
     if (t.remainTime <= 1) {
-        clearInterval(timerupdate);
-        el.innerHTML = finalMessage;
+      clearInterval(timerupdate);
+      el.innerHTML = finalMessage;
     }
-    }, 1000)
+  }, 1000);
 };
 
-countdown('Sep 21 2024 7:20:00 GMT-0300', 'contador', 'LLEGÓ LA JORNADA!' )
+countdown("Sep 21 2024 7:20:00 GMT-0300", "contador", "LLEGÓ LA JORNADA!");
